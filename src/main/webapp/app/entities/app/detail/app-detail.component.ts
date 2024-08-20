6aec32583d798efa85500f6aaad1b143c61a5025
camenduru <camenduru@gmail.com>
2024-08-20 22:00:07 +0000

    ROLE_PAID

--- src/main/webapp/app/entities/app/detail/app-detail.component.ts
+++ src/main/webapp/app/entities/app/detail/app-detail.component.ts
@@ -288,7 +288,7 @@ export class AppDetailComponent implements OnInit, OnDestroy {
   protected onAppResponseSuccess(response: AppEntityArrayResponseType): void {
     this.fillComponentAttributesFromAppResponseHeader(response.headers);
     const dataFromBody = this.fillComponentAttributesFromAppResponseBody(response.body);
-    this.apps = dataFromBody.filter(item => item.isActive === true).reverse();
+    this.apps = dataFromBody.filter(item => item.isActive === true);
     const app = this.app();
     const jsonSchema = app?.schema ? JSON.parse(app.schema) : null;
     this.activeSchema = jsonSchema as unknown as ISchema;
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -317,7 +317,7 @@ export default class HomeComponent implements OnInit, OnDestroy {
   protected onAppResponseSuccess(response: AppEntityArrayResponseType): void {
     this.fillComponentAttributesFromAppResponseHeader(response.headers);
     const dataFromBody = this.fillComponentAttributesFromAppResponseBody(response.body);
-    this.apps = dataFromBody.filter(item => item.isActive === true).reverse();
+    this.apps = dataFromBody.filter(item => item.isActive === true);
     const app = this.apps.find(item => item.isDefault === true);
     const jsonSchema = app?.schema ? JSON.parse(app.schema) : null;
     this.activeSchema = jsonSchema as unknown as ISchema;
