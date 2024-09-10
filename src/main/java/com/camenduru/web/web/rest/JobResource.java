33a0d6e1c675df52a6b4744cbad1b5f77b9ad004
camenduru <camenduru@gmail.com>
2024-09-10 21:27:03 +0000

    itch.io not happy

--- src/main/java/com/camenduru/web/web/rest/JobResource.java
+++ src/main/java/com/camenduru/web/web/rest/JobResource.java
@@ -302,8 +302,7 @@ public class JobResource {
                     Oops! Your balance is insufficient. If you want a daily wallet balance of
                     <span class='text-info' style='font-weight: bold;'>%s</span> ($%s/month), please subscribe via
                     <a class='text-info' style='font-weight: bold;' href='https://github.com/sponsors/camenduru'>GitHub Sponsors</a>,
-                    <a class='text-info' style='font-weight: bold;' href='https://www.patreon.com/camenduru'>Patreon</a>, or purchase a
-                    <a class='text-info' style='font-weight: bold;' href='https://camenduru.itch.io'>Tost Wallet Code</a>.
+                    <a class='text-info' style='font-weight: bold;' href='https://www.patreon.com/camenduru'>Patreon</a>, or purchase a Tost Wallet Code.
                 """,
                 camenduruWebPaidTotal,
                 camenduruWebMinTotal
